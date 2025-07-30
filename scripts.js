cf969ba8d035ad922ed8a05df77929510de352fb
Kevin Candeloni <kevincandeloni@gmail.com>
2025-07-30 01:14:19 -0300

    adicionada a função camelCase

--- scripts.js
+++ scripts.js
@@ -1,14 +1,32 @@
 function toUppercase() {
       const textarea = document.getElementById("texto");
       textarea.value = textarea.value.toUpperCase();
-    }
-    function toLowercase() {
-      const textarea = document.getElementById("texto");
-      textarea.value = textarea.value.toLowerCase();
-    }
-    function capitalizeWords() {
-      const textarea = document.getElementById("texto");
-      textarea.value = textarea.value
-        .toLowerCase()
-        .replace(/\b(\w)/g, (s) => s.toUpperCase());
-    }
+}
+function toLowercase() {
+    const textarea = document.getElementById("texto");
+    textarea.value = textarea.value.toLowerCase();
+}
+function capitalizeWords() {
+    const textarea = document.getElementById("texto");
+    textarea.value = textarea.value
+    .toLowerCase()
+    .replace(/\b(\w)/g, (s) => s.toUpperCase());
+}
+function toCamelCase() {
+  const textarea = document.getElementById("texto");
+  let words = textarea.value
+    .toLowerCase()
+    .trim()
+    .split(/[^a-zA-Z0-9]+/);
+
+  if (words.length === 0) {
+    textarea.value = "";
+    return;
+  }
+
+  let camelCaseText = words[0];
+  for (let i = 1; i < words.length; i++) {
+    camelCaseText += words[i].charAt(0).toUpperCase() + words[i].slice(1);
+  }
+  textarea.value = camelCaseText;
+}
